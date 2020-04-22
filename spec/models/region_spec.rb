8fc509fc1e52656744724ae932afb466a5d99a1e
Kimber Rabago <rabagok@oregonstate.edu>
2020-04-21 21:48:22 -0700

    Passing 'relationships' test 'has many tickets'

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 	
-	let(:region) { Region.new }
-		
-	it "has a name" do
-		expect(region).to respond_to(:name) 	
-	end
+	let(:region) { Region.new(name: 'FAKE') }
 	
+	describe "attributes" do
+		it "has a name" do
+			expect(region).to respond_to(:name)
+		end
+	end
+
+	describe "relationships" do	
+		it "has many tickets" do	
+			expect(region).to have_many(:tickets)
+		end
+	end
 end
+
+
