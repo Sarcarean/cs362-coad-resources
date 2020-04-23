acfa93d910b17515d563c51b6a8b04018e5e3c4d
R. Harris <43601394+Sarcarean@users.noreply.github.com>
2020-04-23 08:57:34 -0700

    add relationship test to organization model

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+	#let(:organization) { Organization.new(name: = FAKE, ) }
+	
+	describe 'relationship' do
+		it "has an email" do
+			expect(organization).to respond_to(:email)
+		end
+		it "has a name" do
+			expect(organization).to respond_to(:name)
+		end
+		it "has a status" do
+			expect(organization).to respond_to(:status)
+		end
+		it "has a primary name" do
+			expect(organization).to respond_to(:primary_name)
+		end
+		it "has a seconary name" do
+			expect(organization).to respond_to(:secondary_name)
+		end
+		it "has a secondary phone" do
+			expect(organization).to respond_to(:secondary_phone)
+		end
+	end
+
 end
