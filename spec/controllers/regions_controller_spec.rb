93ca1c7ab599250f7824b2eb6fa5c6f5e46e0510
R. Harris <43601394+Sarcarean@users.noreply.github.com>
2020-05-05 08:45:12 -0700

    Added controller spec

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
-
+  
+  context 'As a public user' do
+    # Check to make sure users cannot do anything
+    #Redirect to sign in page
+	describe '/regions' do
+	  specify { expect(get(:index)).to redirect_to(new_user_session_path) }
+	end
+  end
+  
+  context 'As an organization user' do
+	# Limited access, send to dashboard
+	let(:user) { create(:user) }	
+	before(:each) { sign_in(user) }
+	
+	describe 'GET #index' do
+	  specify { expect(get(:index)).to redirect_to(dashboard_path) }
+	end
+		
+  end
+  
+  context 'As an admin' do
+	let(:admin_user) { create(:user, :admin) }	
+	before(:each) { sign_in(admin_user) }
+	
+	describe 'GET #index' do
+	  specify { expect(get(:index)).to be_successful }
+	end
+	
+  end
+  
 end
