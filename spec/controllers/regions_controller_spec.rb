0c41ccf6c93d9ac09f740a70b4fb4a4bf76a74c7
Kimber Rabago <rabagok@oregonstate.edu>
2020-05-06 19:44:02 -0700

    Regions public user controller tests covered

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe RegionsController, type: :controller do
 	  specify { expect(get(:index)).to redirect_to(new_user_session_path) }
 	end
 
-	# describe 'GET #show' do
-	# 	specify { expect(get(:show)).to redirect_to(new_user_session_path) }
-	# end
+	describe 'GET #show' do
+		specify { expect(get(:show, params: { id: 'FAKE' } )).to redirect_to(new_user_session_path) }
+	end
 
 	describe 'GET #new' do	 
 		specify { expect(get(:new)).to redirect_to(new_user_session_path) }
@@ -21,9 +21,17 @@ RSpec.describe RegionsController, type: :controller do
 		specify { expect(post(:create)).to redirect_to(new_user_session_path) }
 	end
 
-	# describe 'GET #edit' do
-	# 	specify { expect(get(:edit)).to redirect_to(new_user_session_path) }
-	# end
+	describe 'GET #edit' do
+		specify { expect(get(:edit, params: { id: 'FAKE' } )).to redirect_to(new_user_session_path) }
+	end
+
+	describe 'PATCH #update' do
+		specify { expect(patch(:update, params: { id: 'FAKE' } )).to redirect_to(new_user_session_path) }
+	end
+
+	describe 'DELETE #destroy' do
+		specify { expect(delete(:destroy, params: { id: 'FAKE' } )).to redirect_to(new_user_session_path) }
+	end
   end
   
   context 'As an organization user' do
