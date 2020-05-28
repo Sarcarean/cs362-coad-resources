61274ab02ae4199d41ce5857e4e3e3749e2ebbd6
Kimber Rabago <rabagok@oregonstate.edu>
2020-05-28 00:21:42 -0700

    Passing public user #new controller test

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
+    context 'As a public user' do
+        describe 'GET #new' do	 
+		    	specify { expect(get(:new)).to be_successful }
+	    end
+    end
 end
