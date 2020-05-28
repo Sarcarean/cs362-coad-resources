9e16515a284f4e7ac09e6ae20b707e7fd962ff01
Kimber Rabago <rabagok@oregonstate.edu>
2020-05-28 00:56:21 -0700

    Passing organization user #new controller test

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -32,5 +32,14 @@ RSpec.describe TicketsController, type: :controller do
 			describe 'DELETE #destroy' do
 				specify { expect(delete(:destroy, params: { id: 'FAKE' } )).to redirect_to(dashboard_path) }
 			end
-    end
+		end
+		
+		context 'As an organization user' do
+			let(:user) { create(:user) }	
+			before(:each) { sign_in(user) }
+
+			describe 'GET #new' do	 
+				specify { expect(get(:new)).to be_successful }
+			end
+		end
 end
