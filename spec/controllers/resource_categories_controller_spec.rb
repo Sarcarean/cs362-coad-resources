850ab712b865c8e4137204486a0d910b57301622
Kimber Rabago <rabagok@oregonstate.edu>
2020-05-27 22:57:34 -0700

    Passing public user #new controller test

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
 
-    context 'As a public user' do
+		context 'As a public user' do
+
       describe 'GET #index' do
 	      specify { expect(get(:index)).to redirect_to(new_user_session_path) }
-			end
+			end	
+
 			describe 'GET #show' do	 
 				specify { expect(get(:show, params: { id: 'FAKE' } )).to redirect_to(new_user_session_path) }
+			end
+
+			describe 'GET #new' do	 
+				specify { expect(get(:new)).to redirect_to(new_user_session_path) }
 			end
     end
 end
