43e06f09ce564eae497ebb598f7b523a3fd7c5d6
Kimber Rabago <rabagok@oregonstate.edu>
2020-05-27 23:16:25 -0700

    Passing organization user #index controller test

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -39,5 +39,14 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 			describe 'DELETE #destroy' do
 				specify { expect(delete(:destroy, params: { id: 'FAKE' } )).to redirect_to(new_user_session_path) }
 			end
-    end
+		end
+		
+		context 'As organization user' do
+			let(:user) { create(:user) }	
+			before(:each) { sign_in(user) }
+
+			describe 'GET #index' do
+	  		specify { expect(get(:index)).to redirect_to(dashboard_path) }
+			end
+		end
 end
