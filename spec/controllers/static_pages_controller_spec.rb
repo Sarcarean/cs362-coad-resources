1794668151d381a8fa7c4591e07d8352db614d82
Kimber Rabago <rabagok@oregonstate.edu>
2020-06-02 00:01:01 -0700

    Passing organization user #index test

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe StaticPagesController, type: :controller do
         end
         
     end
+
+    context 'As an organization user' do
+        let(:user) { create(:user) }	
+        before(:each) { sign_in(user) }
+        
+        specify 'GET #index' do
+		    expect(get(:index)).to be_successful
+	    end
+
+    end
 end
