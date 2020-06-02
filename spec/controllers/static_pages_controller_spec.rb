a6d8e737d447313a1cb1e8f08d5a51e2c36dd4e9
Kimber Rabago <rabagok@oregonstate.edu>
2020-06-01 23:59:15 -0700

    Passing public user #index test

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
+    context 'As a public user' do
+
+        specify 'GET #index' do
+		    expect(get(:index)).to be_successful
+        end
+        
+    end
 end
