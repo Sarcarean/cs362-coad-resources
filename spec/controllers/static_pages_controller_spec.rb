01b483595592e9825e88ab0fcc52c6d5bf270a7c
Kimber Rabago <rabagok@oregonstate.edu>
2020-06-02 00:06:13 -0700

    Passing admin user #index test

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -13,10 +13,20 @@ RSpec.describe StaticPagesController, type: :controller do
     context 'As an organization user' do
         let(:user) { create(:user) }	
         before(:each) { sign_in(user) }
-        
+
         specify 'GET #index' do
 		    expect(get(:index)).to be_successful
 	    end
 
+    end
+
+    context 'As an admin user' do
+        let(:admin_user) { create(:user, :admin) }	
+        before(:each) { sign_in(admin_user) }
+
+        specify 'GET #index' do
+		    expect(get(:index)).to be_successful
+        end
+        
     end
 end
