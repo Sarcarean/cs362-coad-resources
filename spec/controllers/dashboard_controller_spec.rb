fa0f42a4a47c3ca67052a7f017f78fcd10a45771
Kimber Rabago <rabagok@oregonstate.edu>
2020-06-01 23:17:02 -0700

    Passing organization user #index test

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -7,5 +7,16 @@ RSpec.describe DashboardController, type: :controller do
     specify 'GET #index' do
       expect(get(:index)).to redirect_to(new_user_session_path)
     end
+
+  end
+
+  context 'As an organization user' do
+    let(:user) { create(:user) }	
+    before(:each) { sign_in(user) }
+    
+    specify 'GET #index' do
+      expect(get(:index)).to be_successful
+    end
+    
   end
 end
