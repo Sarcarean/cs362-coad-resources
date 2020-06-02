c01893beb9944719824c86ab1384fa820ba65a40
Kimber Rabago <rabagok@oregonstate.edu>
2020-06-01 23:01:21 -0700

    Passing public user #show test

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -15,9 +15,9 @@ RSpec.describe TicketsController, type: :controller do
 			end
 
 			# NoMethodError : admin? 
-			# describe 'GET #show' do
-			#	specify { expect(get(:show, params: { id: 'FAKE' } )).to redirect_to(dashboard_path) }
-			# end
+			describe 'GET #show' do
+			specify { expect(get(:show, params: { id: 'FAKE' } )).to redirect_to(dashboard_path) }
+			end
 
 			describe 'POST #capture' do
 				specify { expect(post(:capture, params: { id: 'FAKE' } )).to redirect_to(dashboard_path) }
