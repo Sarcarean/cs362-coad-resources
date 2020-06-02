e042561be786b8ee374184912339543b4a3e21c6
Kimber Rabago <rabagok@oregonstate.edu>
2020-06-01 23:12:56 -0700

    Passing public user #index test

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
 
+  context 'As a public user' do
+
+    specify 'GET #index' do
+      expect(get(:index)).to redirect_to(new_user_session_path)
+    end
+  end
 end
--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
-		context 'As a public user' do
+	context 'As a public user' do
 			
       describe 'GET #new' do	 
 		    specify { expect(get(:new)).to be_successful }
